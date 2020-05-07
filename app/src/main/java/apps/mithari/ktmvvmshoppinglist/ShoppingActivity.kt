a0cb04cdb82c20805204df45dbef8a793b4524f2
Kapil Tapsi <kapiltapsi@gmail.com>
2020-05-07 19:51:26 +0530

    Entity class (Shopping Item) added for room persistence library
    Signed-off-by: Kapil Tapsi <kapiltapsi@gmail.com>

--- app/src/main/java/apps/mithari/ktmvvmshoppinglist/ShoppingActivity.kt
+++ app/src/main/java/apps/mithari/ktmvvmshoppinglist/ShoppingActivity.kt
@@ -3,10 +3,10 @@ package apps.mithari.ktmvvmshoppinglist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
---
+++ app/src/main/java/apps/mithari/ktmvvmshoppinglist/ShoppingItem.kt
@@ -0,0 +1,19 @@
+package apps.mithari.ktmvvmshoppinglist
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity         //you can set custom table name (tableName = "shopping_items")
+data class ShoppingItem(
+
+    @ColumnInfo(name = "item_name")       // columninfo is optional
+    var name: String,
+
+    @ColumnInfo(name = "item_amount")
+    var amount: Int
+
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
