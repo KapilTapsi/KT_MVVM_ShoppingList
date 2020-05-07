a9ac3534e58292293d324ee7bf6ec0c1993263fe
Kapil Tapsi <kapiltapsi@gmail.com>
2020-05-07 22:53:49 +0530

    Recycler View Added
    Signed-off-by: Kapil Tapsi <kapiltapsi@gmail.com>

---
+++ app/src/main/java/apps/mithari/ktmvvmshoppinglist/other/ShoppingItemAdapter.kt
@@ -0,0 +1,45 @@
+package apps.mithari.ktmvvmshoppinglist.other
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import apps.mithari.ktmvvmshoppinglist.R
+import apps.mithari.ktmvvmshoppinglist.data.db.entities.ShoppingItem
+import apps.mithari.ktmvvmshoppinglist.ui.shoppinglist.ShoppingViewModel
+import kotlinx.android.synthetic.main.shopping_item.view.*
+
+class ShoppingItemAdapter(
+    var items: List<ShoppingItem>,
+    private val viewModel: ShoppingViewModel
+) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+    inner class ShoppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
+        return ShoppingViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val currentShoppingItem = items[position]
+        holder.itemView.tvName.text = currentShoppingItem.name
+        holder.itemView.tvAmount.text = currentShoppingItem.amount.toString()
+        holder.itemView.ivDelete.setOnClickListener { viewModel.delete(currentShoppingItem) }
+        holder.itemView.ivMinus.setOnClickListener {
+            if (currentShoppingItem.amount > 0) {
+                currentShoppingItem.amount--
+                viewModel.upsert(currentShoppingItem)
+            }
+        }
+        holder.itemView.ivPlus.setOnClickListener {
+            currentShoppingItem.amount++
+            viewModel.upsert(currentShoppingItem)
+        }
+    }
+
+}
